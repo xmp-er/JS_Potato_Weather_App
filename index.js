8ada1a0191d041f12fa8e9ddfed831011894ea57
xmp-er <bankofhawkgatsu@gmail.com>
2023-03-03 20:03:20 +0100

    Added the basic functionalities

--- index.js
+++ index.js
@@ -10,7 +10,60 @@ country_checkbox=document.querySelector("[country_checkbox]")
 let API="40fc26f8deafa1f4e81aa304dd5685fc";
 
 //array of checkboxes elements
-let checkboxes=[city_checkbox,state_code,country_checkbox]
+let checkboxes=[city_checkbox,country_checkbox,state_code]
+
+
+//function for respective api-calls based on the boxes checked
+
+//function for city
+async function city_call(){
+    try{
+        let data=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${API}`)
+        let res= await data.json()
+        return res
+    }catch(e){
+        console.log(e);
+    }
+}
+
+//function for country_code
+async function country_call(){
+    try{
+        let data=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city.value},${country_code.value}&appid=${API}`)
+        let res=await data.json()
+        return res
+    }catch(e){
+        console.log(e);
+    }
+}
+
+//function for state_code
+async function state_call(){
+    try{
+        let data=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city.value},${state_code.value},${country_code.value}&appid=${API}`)
+        let res=await data.json()
+        return res
+    }catch(e){
+        console.log(e);
+    }
+}
+
+
+//function call of the WeatherApp API
+async function main_weather_function(temp_checkbox){
+    let len=temp_checkbox.length;
+    if(len==0){
+        alert("Please check one of the boxes");
+        return;
+    }
+    if(len==1){
+        return city_call();
+    }
+    if(len==2){
+        return state_call();
+    }
+    return country_call();
+}
 
 // function for the search button
 search_button.addEventListener('click',function(){
@@ -33,12 +86,3 @@ search_button.addEventListener('click',function(){
     main_weather_function(temp_checkbox)
 })
 
-//function call of the WeatherApp API
-async function main_weather_function(temp_checkbox){
-    let len=temp_checkbox.length;
-    if(len==0){
-        alert("Please check one of the boxes");
-        return;
-    }
-    if()
-}
